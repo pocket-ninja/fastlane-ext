bb8b6322768179f4a965a2c89aa92d66a1a5019d
sroik <vasili.kazhanouski@gmail.com>
2019-09-30 13:45:36 +0300

    fix app release (#am) (#22)

--- lib/fastlane-craft/app_release_manager.rb
+++ lib/fastlane-craft/app_release_manager.rb
@@ -47,6 +47,8 @@ module FastlaneCraft
     end
 
     def upload_to_tf
+      # see more at https://github.com/fastlane/fastlane/issues/15390
+      ENV["DELIVER_ITMSTRANSPORTER_ADDITIONAL_UPLOAD_PARAMETERS"] = "-t DAV"
       cmd = 'fastlane pilot upload --skip_submission --skip_waiting_for_build_processing'
       raise "TF uploading Failed! Command execution error: '#{cmd}'" unless system(cmd)
     end
