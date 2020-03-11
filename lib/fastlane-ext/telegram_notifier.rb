8cd369f663d7445a02c104438f5864df0bb5209e
egor mihneivch <cursed.elfenlaid@gmail.com>
2020-03-11 18:30:26 +0300

    fix tests

--- lib/fastlane-ext/telegram_notifier.rb
+++ lib/fastlane-ext/telegram_notifier.rb
@@ -16,11 +16,11 @@ module FastlaneExt
       @chat_id = chat_id
     end
 
-    def notify(message:, parse_mode: nil, silent: true)
+    def notify(message:, parse_mode: nil, silent: nil)
       uri = URI.parse("https://api.telegram.org/bot#{@bot_api_token}/sendMessage")
       params = { text: message, chat_id: @chat_id }
       params[:parse_mode] = parse_mode unless parse_mode.nil?
-      params[:disable_notification] = silent
+      params[:disable_notification] = silent || true
       Net::HTTP.post_form(uri, params)
     end
   end
