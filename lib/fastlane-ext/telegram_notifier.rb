19c58272d2122bb46526cc25a99530248ac68031
elfenlied <cursed.elfenlaid@gmail.com>
2020-03-11 18:24:29 +0300

    Update telegram_notifier.rb

--- lib/fastlane-ext/telegram_notifier.rb
+++ lib/fastlane-ext/telegram_notifier.rb
@@ -16,10 +16,11 @@ module FastlaneExt
       @chat_id = chat_id
     end
 
-    def notify(message:, parse_mode: nil)
+    def notify(message:, parse_mode: nil, silent: true)
       uri = URI.parse("https://api.telegram.org/bot#{@bot_api_token}/sendMessage")
       params = { text: message, chat_id: @chat_id }
       params[:parse_mode] = parse_mode unless parse_mode.nil?
+      params[:disable_notification] = silent
       Net::HTTP.post_form(uri, params)
     end
   end
